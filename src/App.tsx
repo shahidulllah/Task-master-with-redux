5773ceb447fad9fb4511b01429c515a2b6ecf27f
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-06 11:14:08 +0600

    router created

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import Navbar from './components/layout/Navbar'
+import Navbar from "./components/layout/Navbar";
 
 function App() {
-  
-
   return (
     <div>
       <Navbar></Navbar>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
