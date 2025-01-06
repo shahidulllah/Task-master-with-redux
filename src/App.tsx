e49c93cc9a16546170ab41e135f7fcc2e6c26f4e
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-06 08:54:53 +0600

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Task Master</h1>
+      <div className="card">
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
+      </div>
+    </>
+  )
+}
+
+export default App
