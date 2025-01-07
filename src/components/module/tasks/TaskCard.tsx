ce3c1e0bf5e2277d8b4ae56851da7ee28508e554
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-08 00:05:04 +0600

    card is dynamically working

--- src/components/module/tasks/TaskCard.tsx
+++ src/components/module/tasks/TaskCard.tsx
@@ -1,17 +1,24 @@
 import { ITask } from "@/app/redux/features/task/task.interface";
 import { Button } from "@/components/ui/button";
 import { Checkbox } from "@/components/ui/checkbox";
+import { cn } from "@/lib/utils";
 
 interface IProps {
-    task: ITask
+  task: ITask;
 }
 
-const TaskCard = ({task}: IProps) => {
+const TaskCard = ({ task }: IProps) => {
   return (
     <div className="border px-5 py-3 rounded-md">
       <div className="flex justify-between items-center">
         <div className="flex gap-2 items-center">
-          <div className="size-3 rounded-full bg-green-500"></div>
+          <div
+            className={cn("size-3 rounded-full", {
+              "bg-green-500": task.priority === "Low",
+              "bg-yellow-500": task.priority === "Medium",
+              "bg-red-500": task.priority === "High",
+            })}
+          ></div>
           <h1>{task.title}</h1>
         </div>
         <div className="flex gap-3 items-center">
