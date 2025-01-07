5c94472c85f6cdceb8eff79ceba231b8306c0015
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-07 23:20:08 +0600

    Working on task card

---
+++ src/components/module/tasks/TaskCard.tsx
@@ -0,0 +1,13 @@
+
+
+
+const TaskCard = () => {
+    
+    return (
+        <div className="mx-auto max-w-7xl px-5 mt-5">
+            
+        </div>
+    );
+};
+
+export default TaskCard;
--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,15 +1,21 @@
-import { selectFilter, selectTasks } from "@/app/redux/features/task/taskSlice";
+import {selectTasks } from "@/app/redux/features/task/taskSlice";
 import { useAppSelector } from "@/app/redux/hooks";
+import TaskCard from "@/components/module/tasks/TaskCard";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
 
-  const filter = useAppSelector(selectFilter);
-
-  console.log(tasks, filter);
+  console.log(tasks);
   return (
-    <div>
-      <h1>This is task components</h1>
+    <div className="mx-auto max-w-7xl px-5 mt-5">
+      <div>
+        <h1>Tasks</h1>
+      </div>
+      <div className="space-y-5 mt-5">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </div>
     </div>
   );
 };
