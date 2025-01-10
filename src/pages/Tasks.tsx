6285aa006f505751098ba4b85b5d5af290110964
shahidulllah <mdshahidsumon177@gmail.com>
2025-01-10 19:44:13 +0600

    filter ui with shadcn

--- src/pages/Tasks.tsx
+++ src/pages/Tasks.tsx
@@ -1,7 +1,8 @@
-import {selectTasks } from "@/app/redux/features/task/taskSlice";
+import { selectTasks } from "@/app/redux/features/task/taskSlice";
 import { useAppSelector } from "@/app/redux/hooks";
-import {AddTaskModal } from "@/components/module/tasks/AddTask";
+import { AddTaskModal } from "@/components/module/tasks/AddTask";
 import TaskCard from "@/components/module/tasks/TaskCard";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 const Tasks = () => {
   const tasks = useAppSelector(selectTasks);
@@ -9,16 +10,23 @@ const Tasks = () => {
   console.log(tasks);
   return (
     <div className="mx-auto max-w-7xl px-5 mt-5">
-      <div className="flex justify-between items-center">
-        <h1>Tasks</h1>
-        <AddTaskModal/>
+      <div className="flex justify-end items-center gap-5">
+        <h1 className="mr-auto">Tasks</h1>
+
+        <Tabs>
+          <TabsList className="grid w-full grid-cols-2">
+            <TabsTrigger value="all">All</TabsTrigger>
+            <TabsTrigger value="high">High</TabsTrigger>
+            <TabsTrigger value="medium">Medium</TabsTrigger>
+            <TabsTrigger value="low">Low</TabsTrigger>
+          </TabsList>
+        </Tabs>
+        <AddTaskModal />
       </div>
       <div className="space-y-5 mt-5">
-       {
-        tasks.map(task => (
+        {tasks.map((task) => (
           <TaskCard key={task.id} task={task}></TaskCard>
-        ))
-       }
+        ))}
       </div>
     </div>
   );
